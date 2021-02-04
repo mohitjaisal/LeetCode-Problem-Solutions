646ee7bbbe0ab952141a7e954518f87c395c87cf
Mohit Jaisal <mohitjaisal19@gmail.com>
2021-02-04 23:32:23 +0530

    05.Longest-Palindromic-Substring.cpp

--- 05.Longest-Palindromic-Substring.cpp
+++ 05.Longest-Palindromic-Substring.cpp
@@ -1,25 +1,31 @@
 class Solution {
 public:
-  string longestPalindrome(string s) {
-    int n = s.size();
-    if (n == 0)
-      return "";
-
-    vector<vector<bool>> dp(n, vector<bool>(n, true));
-    // length is length of substring
-    // 1 length substrings are palindrome
-    int startIndex = 0;
-    int ansLength = 1;
-    for (int length = 2; length <= n; length++) {
-      for (int i = 0; i < n && i + length <= n; i++) {
-        int j = i + length - 1;
-        dp[i][j] = (s[i] == s[j] && dp[i + 1][j - 1]);
-        if (dp[i][j]) {
-          startIndex = i;
-          ansLength = length;
+    string longestPalindrome(string s) {
+        int max_len = 0, len = 0, len1 = 0, len2 = 0;
+        int left = 0, right = 0;
+        for(int i = 0; i<s.length(); i++)
+        {
+            len1 = palindrome(s, i, i);
+            len2 = palindrome(s, i, i+1);
+            len = max(len1, len2);
+            if (len > max_len)
+            {
+                max_len = len;
+                left = i - (len-1)/2;
+                right = i + len/2;
+            }
+                
         }
-      }
+        return s.substr(left, max_len);
     }
-    return s.substr(startIndex, ansLength);
-  }
+    
+    int palindrome (string s, int left, int right)
+    {
+        while (left>=0 && right < s.length() && s[left] == s[right])
+        {
+                left--;
+                right++;            
+        }
+        return right - left - 1;
+    }
 };

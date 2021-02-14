d82efda8c57fe9c37bf80f9cd29f993a89aec2ca
Mohit Jaisal <mohitjaisal19@gmail.com>
2021-02-14 22:55:11 +0530

    24.swap-nodes-in-pairs.cpp

---
+++ 24.swap-nodes-in-pairs.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+  ListNode *swapPairs(ListNode *head) {
+    if (!head)
+      return NULL;
+    if (!head->next)
+      return head;
+
+    auto dummy = new ListNode(0);
+    dummy->next = head;
+
+    ListNode *prevNode = dummy;
+
+    while (head && head->next) {
+      auto first = head;
+      auto second = head->next;
+
+      first->next = second->next;
+      prevNode->next = second;
+      second->next = first;
+      prevNode = first;
+      head = first->next;
+    }
+
+    return dummy->next;
+  }
+};
